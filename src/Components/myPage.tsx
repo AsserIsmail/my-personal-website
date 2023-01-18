c37b5d4284e552f274d5da084a0c7c96cd00eaed
Asser Ismail <asserice@gmail.com>
2023-01-18 00:23:11 -0500

    First Commit-Yazan was here!
    Download My react app and hosting it on DigitalOcean and created MyPage which hold start up stuff

---
+++ src/Components/myPage.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { Box, Heading, Text, Button, Center, Container } from '@chakra-ui/react';
+
+export const MyPage = () => {
+
+  return (
+      <Container>
+      <Center>
+        <Box textAlign="center" py={10} px={6}>
+        <Heading
+            display="inline-block"
+            as="h2"
+            size="2xl"
+            bgGradient="linear(to-r, teal.400, teal.600)"
+            backgroundClip="text">
+            Asser Ismail Portfolio
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+            -Coming Soon-
+        </Text>
+        <Text color={'gray.500'} mb={6}>
+            Hello World! I'm Asser Ismail, a software engineer based in the Canada. I specialize in building (and occasionally designing) exceptional websites, applications, and everything in between.
+        </Text>
+
+        </Box>
+    </Center>
+    </Container>
+  );
+}
