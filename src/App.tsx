b72fc665abc68875abd89caff57be99ab5d5cc25
Asser Ismail <asserice@gmail.com>
2023-09-03 13:19:08 -0400

    Fixed About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ export const App = () => (
       {/*<About2 />*/}
       <Spacer h="100px" />
       <Spacer h="100px" />
-      <Spacer h="100px" />
       {/* <Projects />
       <Skills />
       <Testimonials />
